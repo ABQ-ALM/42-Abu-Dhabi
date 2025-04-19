4a90e063fed4006ea2c438480e273e5c083c4e0b
Abdulla Almheiri <abq.cs.cs@gmail.com>
2025-04-19 15:09:23 +0400

    Finished Exercise 08: Implemented ft_sort_int_tab

---
+++ piscine/c/C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalmheir <aalmheir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 14:35:11 by aalmheir          #+#    #+#             */
+/*   Updated: 2025/04/19 15:08:17 by aalmheir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+	int	sorted;
+
+	sorted = 0;
+	while (!sorted)
+	{
+		sorted = 1;
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+				sorted = 0;
+			}
+			i++;
+		}
+	}
+}
