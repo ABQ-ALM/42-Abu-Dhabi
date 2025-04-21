5157bd9a27bc670f0ed9e96cd7daef4adf174f81
Abdulla Almheiri <abq.cs.cs@gmail.com>
2025-04-21 16:40:28 +0400

    C03 ex02 - ft_strcat

---
+++ piscine/c/C03/ex02/ft_strcat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalmheir <aalmheir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 15:47:31 by aalmheir          #+#    #+#             */
+/*   Updated: 2025/04/21 16:39:32 by aalmheir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Takes 2 strings and concatenates the second string onto the first and returns it */
+char	*ft_strcat(char *dest, char *src)
+{
+	char	*str;
+
+	str = dest;
+	while (*str)
+		str++;
+	while (*src)
+	{
+		*str = *src;
+		str++;
+		src++;
+	}
+	*str = '\0';
+	return (dest);
+}
