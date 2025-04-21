2e16b1249a8b2ecd3eb2dd85a254246c8bd2adea
Abdulla Almheiri <abq.cs.cs@gmail.com>
2025-04-21 22:12:02 +0400

    C03 ex05 - ft_strlcat

---
+++ piscine/c/C03/ex05/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalmheir <aalmheir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 20:16:28 by aalmheir          #+#    #+#             */
+/*   Updated: 2025/04/21 21:22:55 by aalmheir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Appends src to dest, returns total length it tried to create */
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+
+	i = 0;
+	dest_len = 0;
+	src_len = 0;
+	while (dest[dest_len] && dest_len < size)
+		dest_len++;
+	while (src[src_len])
+		src_len++;
+	if (dest_len == size)
+		return (size + src_len);
+	while (src[i] && (dest_len + i + 1 < size))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	if (dest_len + i < size)
+		dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
